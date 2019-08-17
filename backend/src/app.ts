6ad907855b332a17ef533574b547d2d7e25c42bf
Stuart Thomson <me@sthom.kiwi>
2019-08-18 11:27:42 +1200

    Add version endpoint

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,17 @@
 import express, { Request, Response } from "express";
+import compression from "compression";
+import helmet from "helmet";
+import { version } from "../package.json";
 
 const app = express();
+app.use(helmet());
+app.use(compression());
+app.disable("x-powered-by");
 
 app.get("/", (req: Request, res: Response) =>
   res.send("Hello World from app.ts!")
 );
+
+app.get("/version", (req: Request, res: Response) => res.send({ version }));
 
 export default app;
