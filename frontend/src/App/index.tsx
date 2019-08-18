7027d36addb2e8a652c1d68c02c4578e512f81b5
Stuart Thomson <me@sthom.kiwi>
2019-08-18 16:49:07 +1200

    Move some frontend files around

--- frontend/src/App/index.tsx
+++ frontend/src/App/index.tsx
@@ -16,7 +16,7 @@ const App: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React {process.env.REACT_APP_SERVER_PATH} foo
         </a>
       </header>
     </div>
