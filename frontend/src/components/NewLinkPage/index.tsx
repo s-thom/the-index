f7816f045e699abb94b2d1187a5330815da1cd9d
Stuart Thomson <me@sthom.kiwi>
2019-08-20 19:47:58 +1200

    Add request to form submission

--- frontend/src/components/NewLinkPage/index.tsx
+++ frontend/src/components/NewLinkPage/index.tsx
@@ -1,21 +1,43 @@
 import React, { useState } from "react";
 import "./index.css";
-import { RouteComponentProps } from "@reach/router";
+import { RouteComponentProps, navigate } from "@reach/router";
 import NewLinkForm from "../NewLinkForm";
+import { addNewLink } from "../../requests";
+import TextButton from "../TextButton";
 
 interface NewLinkPageProps extends RouteComponentProps {}
 
 export default function NewLinkPage(props: NewLinkPageProps) {
   const [submitting, setSubmitting] = useState(false);
+  const [submitError, setSubmitError] = useState<Error | undefined>(undefined);
 
-  function onFormSubmit(url: string, tags: string[]) {
-    console.log("form submitted", url, tags);
+  async function onFormSubmit(url: string, tags: string[]) {
     setSubmitting(true);
+
+    try {
+      const newLinkId = await addNewLink(url, tags);
+      navigate(`/link/${newLinkId}`);
+
+      setSubmitting(false);
+    } catch (err) {
+      console.error("[NewLinkPage submit]", err);
+      setSubmitError(err);
+      setSubmitting(false);
+    }
   }
 
   return (
     <div className="NewLinkPage">
       <h2 className="NewLinkPage-heading">New Link</h2>
+      {submitError && (
+        <div className="NewLinkPage-error">
+          <p className="NewLinkPage-error-text">
+            There was an error while creating you new link. There's not much you
+            can do, really.
+          </p>
+          <TextButton type="button">Re-enable form</TextButton>
+        </div>
+      )}
       <NewLinkForm onSubmit={onFormSubmit} disabled={submitting} />
     </div>
   );
--- frontend/src/requests.ts
+++ frontend/src/requests.ts
@@ -1,16 +1,25 @@
-import axios from 'axios';
-import { LinkDetail } from './types';
+import axios from "axios";
+import { LinkDetail } from "./types";
 
 const SERVER_HOST = process.env.REACT_APP_SERVER_PATH;
 
 interface SearchByTagResponse {
-  links: LinkDetail[]
+  links: LinkDetail[];
 }
 
 export async function searchByTag(tags: string[]): Promise<LinkDetail[]> {
   const response = await axios.post(`${SERVER_HOST}/search`, {
-    tags,
+    tags
   });
 
   return response.data.links;
+}
+
+export async function addNewLink(url: string, tags: string[]): Promise<string> {
+  const response = await axios.post(`${SERVER_HOST}/links`, {
+    url,
+    tags
+  });
+
+  return response.data.id;
 }
