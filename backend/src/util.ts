12e41a350510fc4086b10a9b9070528e4eb49cf7
Stuart Thomson <me@sthom.kiwi>
2019-08-21 18:03:23 +1200

    Add function to wrap Promises as express callbacks

---
+++ backend/src/util.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from "express";
+
+/**
+ * Wraps a Promise function to work with Express
+ * @param routeFn Route to call
+ */
+export function wrapPromiseRoute(
+  routeFn: (requestBody: any, request: Request) => Promise<any>
+) {
+  return async (request: Request, response: Response) => {
+    try {
+      const responseData = await routeFn(request.body, request);
+      response.json(responseData);
+    } catch (err) {
+      let statusCode = 500;
+      let message = "Internal server error";
+
+      if (!(err === null || err === undefined)) {
+        if (err.status) {
+          statusCode = err.status;
+        }
+        if (err.message) {
+          message = err.message;
+        }
+      }
+
+      response.status(statusCode).json({ message });
+    }
+  };
+}
+
+export class StatusError extends Error {
+  public readonly status: number;
+
+  constructor(status: number, message: string) {
+    super(message);
+
+    this.status = status;
+  }
+}
