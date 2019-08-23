05d60beac50ba1824e04bacdb30180cd6d15c2dc
Stuart Thomson <me@sthom.kiwi>
2019-08-24 11:26:35 +1200

    Add authorization header when possible

--- frontend/src/requests.ts
+++ frontend/src/requests.ts
@@ -1,7 +1,19 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { LinkDetail } from "./types";
 
 const SERVER_HOST = process.env.REACT_APP_SERVER_PATH;
+
+interface LinkDetailResponse {
+  link: LinkDetail;
+}
+
+interface SearchResponse {
+  links: LinkDetail[];
+}
+
+interface NewLinkResponse {
+  id: string;
+}
 
 export default class Requester {
   private token: string | null = null;
@@ -14,26 +26,51 @@ export default class Requester {
     this.setTokenFn = setToken;
   }
 
-  async getLinkById(id: string): Promise<LinkDetail> {
-    const response = await axios.get(`${SERVER_HOST}/links/${id}`);
+  private getConfig() {
+    const headers: { [x: string]: string } = {};
+    if (this.token) {
+      headers.Authorization = `Bearer ${this.token}`;
+    }
 
-    return response.data.link;
+    return {
+      withCredentials: true,
+      headers
+    } as AxiosRequestConfig;
+  }
+
+  private async get<T>(path: string) {
+    const response = await axios.get<T>(path, this.getConfig());
+
+    return response.data;
+  }
+
+  private async post<T>(path: string, data: any) {
+    const response = await axios.post<T>(path, data, this.getConfig());
+
+    return response.data;
+  }
+
+  async getLinkById(id: string): Promise<LinkDetail> {
+    const data = await this.get<LinkDetailResponse>(
+      `${SERVER_HOST}/links/${id}`
+    );
+    return data.link;
   }
 
   async searchByTag(tags: string[]): Promise<LinkDetail[]> {
-    const response = await axios.post(`${SERVER_HOST}/search`, {
+    const data = await this.post<SearchResponse>(`${SERVER_HOST}/search`, {
       tags
     });
 
-    return response.data.links;
+    return data.links;
   }
 
   async addNewLink(url: string, tags: string[]): Promise<string> {
-    const response = await axios.post(`${SERVER_HOST}/links`, {
+    const data = await this.post<NewLinkResponse>(`${SERVER_HOST}/links`, {
       url,
       tags
     });
 
-    return response.data.id;
+    return data.id;
   }
 }
