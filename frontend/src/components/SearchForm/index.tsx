dcd006bb0cdb1646888747353f0843b53705c3a1
Stuart Thomson <me@sthom.kiwi>
2019-08-25 09:29:58 +1200

    Move tags form into a new component

--- frontend/src/components/SearchForm/index.tsx
+++ frontend/src/components/SearchForm/index.tsx
@@ -3,63 +3,20 @@ import "./index.css";
 import { getParamAsArray, setParam } from "../../util/getParam";
 import { deduplicate } from "../../util/array";
 import TextButton from "../TextButton";
+import TagsForm from "../TagsForm";
 
 interface SearchFormProps {}
 
 export default function SearchForm(props: SearchFormProps) {
-  const [inputVal, setInputVal] = useState("");
-
   const tags = getParamAsArray("t");
 
-  function addInputAsTag() {
-    tags.push(inputVal);
-    const deduped = deduplicate(tags);
-    setParam("t", deduped);
-  }
-
-  function removeTag(tag: string) {
-    const filtered = tags.filter(t => t !== tag);
-    setParam("t", filtered);
-  }
-
-  function onInputChange(event: React.FormEvent<HTMLInputElement>) {
-    setInputVal(event.currentTarget.value);
-  }
-
-  function onInputKeyPress(event: React.KeyboardEvent) {
-    if (event.key === "Enter" || event.key === " ") {
-      addInputAsTag();
-      setInputVal("");
-    }
+  function onTagsChange(newTags: string[]) {
+    setParam("t", newTags);
   }
 
   return (
     <div className="SearchForm">
-      <div className="SearchForm-input-container">
-        <input
-          className="SearchForm-input"
-          id="add-tag"
-          name="add-tag"
-          placeholder="Add tag"
-          value={inputVal}
-          onChange={onInputChange}
-          onKeyPress={onInputKeyPress}
-        />
-      </div>
-      <ul className="SearchForm-tag-list">
-        {tags.map(tag => (
-          <li className="SearchForm-tag" key={tag}>
-            <TextButton
-              onClick={() => removeTag(tag)}
-              title="Remove"
-              aria-label={`Remove ${tag}`}
-            >
-              -
-            </TextButton>{" "}
-            {tag}
-          </li>
-        ))}
-      </ul>
+      <TagsForm tags={tags} onTagsChange={onTagsChange} />
     </div>
   );
 }
---
+++ frontend/src/components/TagsForm/index.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import "./index.css";
+import { getParamAsArray, setParam } from "../../util/getParam";
+import { deduplicate } from "../../util/array";
+import TextButton from "../TextButton";
+
+interface TagsFormProps {
+  tags: string[];
+  onTagsChange?: (tags: string[]) => void;
+}
+
+export default function TagsForm({ tags, onTagsChange }: TagsFormProps) {
+  const [inputVal, setInputVal] = useState("");
+
+  function addInputAsTag() {
+    tags.push(inputVal);
+    const deduped = deduplicate(tags);
+    if (onTagsChange) {
+      onTagsChange(deduped);
+    }
+  }
+
+  function removeTag(tag: string) {
+    const filtered = tags.filter(t => t !== tag);
+    if (onTagsChange) {
+      onTagsChange(filtered);
+    }
+  }
+
+  function onInputChange(event: React.FormEvent<HTMLInputElement>) {
+    setInputVal(event.currentTarget.value);
+  }
+
+  function onInputKeyPress(event: React.KeyboardEvent) {
+    if (event.key === "Enter" || event.key === " ") {
+      addInputAsTag();
+      setInputVal("");
+    }
+  }
+
+  return (
+    <div className="TagsForm">
+      <div className="TagsForm-input-container">
+        <input
+          className="TagsForm-input"
+          id="add-tag"
+          name="add-tag"
+          placeholder="Add tag"
+          value={inputVal}
+          onChange={onInputChange}
+          onKeyPress={onInputKeyPress}
+        />
+      </div>
+      <ul className="TagsForm-tag-list">
+        {tags.map(tag => (
+          <li className="TagsForm-tag" key={tag}>
+            <TextButton
+              onClick={() => removeTag(tag)}
+              title="Remove"
+              aria-label={`Remove ${tag}`}
+            >
+              -
+            </TextButton>{" "}
+            {tag}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
