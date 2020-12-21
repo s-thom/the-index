7f6480c157f1d1a65b942bb7b5227f68440e597b
Stuart Thomson <me@sthom.kiwi>
2020-12-21 17:47:17 +1300

    Remove header from logged out view

--- packages/frontend/src/App/index.tsx
+++ packages/frontend/src/App/index.tsx
@@ -1,15 +1,23 @@
+import { Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import BodyArea from '../components/BodyArea';
 import Header from '../components/Header';
+import LoadingPage from '../components/LoadingPage';
+import LoggedOutApp from '../components/LoggedOutApp';
+import AuthorizationRoot from '../context/AuthorizationContext';
 import './index.css';
 
 export default function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Header />
-        <BodyArea />
-      </BrowserRouter>
+      <Suspense fallback={<LoadingPage />}>
+        <BrowserRouter>
+          <AuthorizationRoot fallback={<LoggedOutApp />}>
+            <Header />
+            <BodyArea />
+          </AuthorizationRoot>
+        </BrowserRouter>
+      </Suspense>
     </div>
   );
 }
--- packages/frontend/src/components/BodyArea/index.tsx
+++ packages/frontend/src/components/BodyArea/index.tsx
@@ -1,17 +1,13 @@
 import { Suspense } from 'react';
-import AuthorizationRoot from '../../context/AuthorizationContext';
 import LoadingPage from '../LoadingPage';
 import LoggedInApp from '../LoggedInApp';
-import LoggedOutApp from '../LoggedOutApp';
 import './index.css';
 
 export default function BodyArea() {
   return (
     <div className="BodyArea">
       <Suspense fallback={<LoadingPage />}>
-        <AuthorizationRoot fallback={<LoggedOutApp />}>
-          <LoggedInApp />
-        </AuthorizationRoot>
+        <LoggedInApp />
       </Suspense>
     </div>
   );
