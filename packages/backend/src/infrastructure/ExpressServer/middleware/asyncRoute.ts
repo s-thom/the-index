87f935f25ac552a219ae809206a96408838970db
Stuart Thomson <me@sthom.kiwi>
2020-12-26 12:14:39 +1300

    Add query validation to asyncRoute unitity

--- packages/backend/src/infrastructure/ExpressServer/middleware/asyncRoute.ts
+++ packages/backend/src/infrastructure/ExpressServer/middleware/asyncRoute.ts
@@ -11,18 +11,18 @@ import LoggerImpl from '../../Logger/LoggerImpl';
 const loggerService = Container.get<ILogger>(LoggerImpl);
 const logger = loggerService.child('Express.asyncRoute');
 
-function transformError(error: Error): ApiError | MultipleError {
+function transformError(error: Error, type: string): ApiError | MultipleError {
   if (error instanceof Yup.ValidationError) {
     if (error.inner.length > 0) {
       // If the error contains multiple errors, transform each of them
-      return new MultipleError(error.inner.map(transformError));
+      return new MultipleError(error.inner.map((inner) => transformError(inner, type)));
     }
 
     // Transform the error
     return new InvalidRequestError({
       message: error.message,
       safeMessage: 'Invalid request',
-      meta: { path: error.path },
+      meta: { type, path: error.path },
     });
   }
 
@@ -31,27 +31,46 @@ function transformError(error: Error): ApiError | MultipleError {
   return new UnknownError({ message: error.message });
 }
 
-export interface RouteValidation<RequestType, ParamsType> {
+export interface RouteValidation<RequestType, ParamsType, QueryType> {
   params?: ParamsType extends void ? undefined : Yup.SchemaOf<ParamsType>;
+  query?: QueryType extends void ? undefined : Yup.SchemaOf<QueryType>;
   body?: RequestType extends void ? undefined : Yup.SchemaOf<RequestType>;
 }
 
-async function validateSchemas<RequestType, ParamsType>(
-  req: Request<unknown, any, unknown>,
-  validation: RouteValidation<RequestType, ParamsType>,
+async function validateSchemas<RequestType, ParamsType, QueryType>(
+  req: Request<unknown, any, unknown, unknown>,
+  validation: RouteValidation<RequestType, ParamsType, QueryType>,
 ): Promise<{
   params: ParamsType extends void ? unknown : ParamsType;
+  query: QueryType extends void ? unknown : QueryType;
   body: RequestType extends void ? unknown : RequestType;
 }> {
-  let { params, body } = req;
+  let { params, body, query } = req;
   const errors: Error[] = [];
 
   if (validation.params) {
     try {
-      params = await validation.params.validate(req.params, { abortEarly: false });
+      const parsedParams = await validation.params.validate(req.params, { abortEarly: false });
+      if (parsedParams) {
+        params = parsedParams;
+      }
     } catch (error: unknown) {
       if (error instanceof Error) {
-        errors.push(error);
+        errors.push(transformError(error, 'path'));
+      } else {
+        throw error;
+      }
+    }
+  }
+  if (validation.query) {
+    try {
+      const parsedQuery = await validation.query.validate(req.query, { abortEarly: false });
+      if (parsedQuery) {
+        query = parsedQuery;
+      }
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        errors.push(transformError(error, 'query'));
       } else {
         throw error;
       }
@@ -59,10 +78,13 @@ async function validateSchemas<RequestType, ParamsType>(
   }
   if (validation.body) {
     try {
-      body = await validation.body.validate(req.body, { abortEarly: false });
+      const parsedBody = await validation.body.validate(req.body, { abortEarly: false });
+      if (parsedBody) {
+        body = parsedBody;
+      }
     } catch (error: unknown) {
       if (error instanceof Error) {
-        errors.push(error);
+        errors.push(transformError(error, 'body'));
       } else {
         throw error;
       }
@@ -70,17 +92,18 @@ async function validateSchemas<RequestType, ParamsType>(
   }
 
   if (errors.length > 0) {
-    throw new MultipleError(errors.map(transformError));
+    throw new MultipleError(errors);
   }
 
   return {
     params: params as any,
+    query: query as any,
     body: body as any,
   };
 }
 
-export type AsyncRouteHandler<RequestType, ResponseType, ParamsType> = (
-  req: Request<ParamsType, ResponseType, RequestType>,
+export type AsyncRouteHandler<RequestType, ResponseType, ParamsType, QueryType> = (
+  req: Request<ParamsType, ResponseType, RequestType, QueryType>,
   res: Response<ResponseType>,
   next: NextFunction,
 ) => Promise<ResponseType>;
@@ -88,19 +111,24 @@ export type AsyncRouteHandler<RequestType, ResponseType, ParamsType> = (
 export default function asyncRoute<
   RequestType = unknown,
   ResponseType = unknown,
-  ParamsType = import('express-serve-static-core').ParamsDictionary
+  ParamsType = import('express-serve-static-core').ParamsDictionary,
+  QueryType = import('qs').ParsedQs
 >(
-  validation: RouteValidation<RequestType, ParamsType>,
-  handler: AsyncRouteHandler<RequestType, ResponseType, ParamsType>,
-): RequestHandler<ParamsType, ResponseType, RequestType> {
+  validation: RouteValidation<RequestType, ParamsType, QueryType>,
+  handler: AsyncRouteHandler<RequestType, ResponseType, ParamsType, QueryType>,
+): RequestHandler<ParamsType, ResponseType, RequestType, QueryType> {
   return async (req, res, next) => {
     try {
       // Validate schema
-      const { params, body } = await validateSchemas(req, validation);
+      const { params, body, query } = await validateSchemas(req, validation);
       // If there were results of validation, add to request
       if (params) {
         (req as any).originalParams = req.params;
         req.params = params as ParamsType;
+      }
+      if (query) {
+        (req as any).originalQuery = req.query;
+        req.query = query as QueryType;
       }
       if (body) {
         (req as any).originalBody = req.body;
