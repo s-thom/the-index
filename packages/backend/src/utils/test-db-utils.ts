b9554ca48460887ff91680d2418b834449a4494e
Stuart Thomson <me@sthom.kiwi>
2020-12-27 11:18:53 +1300

    Do mock data generation without faker
    Uniqueness became a problem pretty quickly

--- packages/backend/src/utils/test-db-utils.ts
+++ packages/backend/src/utils/test-db-utils.ts
@@ -1,42 +1,16 @@
-import { getConnectionManager } from 'typeorm';
-// This file should not be imported from outside of test files
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { define, factory, useRefreshDatabase } from 'typeorm-seeding';
+import { ExtSnowflakeGenerator } from 'extended-snowflake';
+import { Connection, getConnectionManager } from 'typeorm';
 import LinkModel from '../app/Links/repository/LinkModel.entity';
 import TagModel from '../app/Tags/repository/TagModel.entity';
 import UserModel from '../app/Users/repository/UserModel.entity';
 import TypeOrmServiceImpl from '../services/TypeOrmService/TypeOrmServiceImpl';
-import { mockConfigService, mockLoggerService } from './test-utils';
+import { mockConfigService, mockLogger } from './test-utils';
 
-define(UserModel, (faker, ctx: Partial<UserModel> | undefined) => {
-  const user = new UserModel();
-  user.id = ctx?.id ?? faker.random.number(1000000);
-  user.name = ctx?.name ?? faker.name.firstName().toLowerCase();
-  user.created = ctx?.created ?? faker.date.past(10);
-  user.updated = ctx?.updated;
-  user.deleted = ctx?.deleted;
-  return user;
-});
-
-define(TagModel, (faker, ctx: Partial<TagModel> | undefined) => {
-  const tag = new TagModel();
-  tag.id = ctx?.id ?? faker.random.number(1000000);
-  tag.name = ctx?.name ?? faker.lorem.word();
-  tag.user = ctx?.user ?? ((null as unknown) as UserModel);
-  return tag;
-});
-
-define(LinkModel, (faker, ctx: Partial<LinkModel> | undefined) => {
-  const link = new LinkModel();
-  link.id = ctx?.id ?? faker.random.number(1000000);
-  link.reference = ctx?.reference ?? faker.random.hexaDecimal(18).toUpperCase();
-  link.url = ctx?.url ?? faker.internet.url();
-  link.tags = ctx?.tags ?? ((null as unknown) as TagModel[]);
-  link.user = ctx?.user ?? ((null as unknown) as UserModel);
-  link.updated = ctx?.updated;
-  link.deleted = ctx?.deleted;
-  return link;
-});
+// #region Utilities
+const generator = new ExtSnowflakeGenerator(0);
+function generateString() {
+  return generator.next();
+}
 
 /**
  * Takes a random selection of items from an array
@@ -60,29 +34,93 @@ function takeRandom<T>(arr: T[], n: number): T[] {
   return outArray;
 }
 
-export const typeOrmService = new TypeOrmServiceImpl(mockLoggerService, mockConfigService);
-jest.spyOn(typeOrmService, 'start');
-jest.spyOn(typeOrmService, 'getRepository');
+/**
+ * Runs a callback the specified number of times, returning the result
+ * @param n Number of times to run the callback
+ * @param fn Callback to run
+ */
+function doTimes<T>(n: number, fn: (i: number) => T): T[] {
+  return [...Array(n)].map((_, i) => fn(i));
+}
+// #endregion
+
+// #region make<DataType> function
+function makeUser(overrides?: Partial<UserModel>): UserModel {
+  const user = new UserModel();
+  user.id = undefined as any;
+  user.name = overrides?.name ?? generateString();
+  user.created = overrides?.created ?? new Date('2020-01-01T00:00:00.000Z');
+  user.updated = overrides?.updated;
+  user.deleted = overrides?.deleted;
+  return user;
+}
+
+function makeTag(overrides?: Partial<TagModel>): TagModel {
+  const tag = new TagModel();
+  tag.id = undefined as any;
+  tag.name = overrides?.name ?? generateString();
+  tag.user = overrides?.user ?? (undefined as any);
+  return tag;
+}
+
+function makeLink(overrides?: Partial<LinkModel>): LinkModel {
+  const link = new LinkModel();
+  link.id = undefined as any;
+  link.reference = overrides?.reference ?? generateString();
+  link.url = overrides?.url ?? `https://example.com#${generateString()}`;
+  link.tags = overrides?.tags ?? (undefined as any);
+  link.user = overrides?.user ?? (undefined as any);
+  link.created = overrides?.created ?? new Date('2020-01-01T00:00:00.000Z');
+  link.updated = overrides?.updated;
+  link.deleted = overrides?.deleted;
+  return link;
+}
+// #endregion
+
+export const mockTypeOrmService = new TypeOrmServiceImpl(
+  // Since the constructor is called before the functions have their mock implementations,
+  // we need to manually mock the logger instance in
+  { child: () => mockLogger, get: () => mockLogger },
+  mockConfigService,
+);
+
+export function getTestConnection() {
+  const connection = (mockTypeOrmService as any).connection as Connection;
+  return connection;
+}
 
 export async function seedDatabase() {
-  await typeOrmService.start();
-  await useRefreshDatabase({ connection: 'test' });
-  const users = await factory(UserModel)().createMany(10);
-  const useDataPromises = users.map(async (user) => {
-    const tags = await factory(TagModel)({ user }).createMany(10);
-    const links = await factory(LinkModel)({ user })
-      .map(async (model) => {
-        // The aim of the mapper is to specifically mutate the value
-        // eslint-disable-next-line no-param-reassign
-        model.tags = takeRandom(tags, 3);
-        return model;
-      })
-      .createMany(50);
+  const data = doTimes(10, (index) => makeUser({ id: index + 1 })).map((user) => {
+    const tags = doTimes(10, () => makeTag({ user }));
+    const links = doTimes(50, () => makeLink({ user, tags: takeRandom(tags, 3) }));
 
     return { user, tags, links };
   });
-  await Promise.all(useDataPromises);
+
+  // Extract data from created models
+  const allUsers: UserModel[] = [];
+  const allTags: TagModel[] = [];
+  const allLinks: LinkModel[] = [];
+  for (const item of data) {
+    allUsers.push(item.user);
+    allTags.push(...item.tags);
+    allLinks.push(...item.links);
+  }
+
+  // Save all data to the database
+  const connection = getTestConnection();
+  await connection.getRepository(UserModel).save(allUsers);
+  await connection.getRepository(TagModel).save(allTags, { chunk: 50 });
+  await connection.getRepository(LinkModel).save(allLinks, { chunk: 50 });
 }
+
+beforeEach(async () => {
+  // Seed database
+  // This is a bit of a lengthy operation, which is why the db utils have been split into this file.
+  // Only the <Entity>Respository classes should require this in their tests.
+  await mockTypeOrmService.start();
+  await seedDatabase();
+});
 
 // This is a clone of the cleanup effect in `src/services/TypeOrmService/__tests__/TypeOrmServiceImpl.ts`, and should stay in sync
 afterEach(async () => {
