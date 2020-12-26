1ca773b9db7e375a713233842605731a726d688d
Stuart Thomson <me@sthom.kiwi>
2020-12-26 17:35:19 +1300

    Remove old code

--- packages/backend/src/infrastructure/ExpressServer/controllers/ControllerImpl.ts
+++ packages/backend/src/infrastructure/ExpressServer/controllers/ControllerImpl.ts
@@ -3,7 +3,6 @@ import { Inject, Service } from 'typedi';
 import ILogger, { Logger } from '../../Logger/Logger';
 import LoggerImpl from '../../Logger/LoggerImpl';
 import IController from './Controller';
-import OldControllerImpl from './old';
 import V2ControllerImpl from './v2';
 
 @Service()
@@ -13,7 +12,6 @@ export default class ControllerImpl implements IController {
   constructor(
     @Inject(() => LoggerImpl) private readonly logger: ILogger,
     @Inject(() => V2ControllerImpl) private readonly v2Controller: IController,
-    @Inject(() => OldControllerImpl) private readonly oldController: IController,
   ) {
     this.log = this.logger.child('Controller');
   }
@@ -21,7 +19,6 @@ export default class ControllerImpl implements IController {
   router() {
     const router = Router();
 
-    router.use('/', this.oldController.router());
     router.use('/v2', this.v2Controller.router());
 
     return router;
